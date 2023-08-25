d3f7ae4fedca3cdeafd773d11c7d54101f39e330
Victor Hugo Martínez González <le19080710@itmerida.edu.mx>
2023-08-24 19:12:04 -0600

    update

--- src/main/java/pacman/RobotConnection.java
+++ src/main/java/pacman/RobotConnection.java
@@ -5,6 +5,7 @@ import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Map;
+import java.util.Random;
 
 import pacman.game.Constants.GHOST;
 import pacman.game.Constants.MOVE;
@@ -23,6 +24,7 @@ public class RobotConnection {
 			ServerSocket server = new ServerSocket(5050);
 			System.out.println("Servidor creado");
 			client = server.accept();
+			System.out.println(client.getInetAddress());
 			System.out.println("Conexcion aceptada");
 			output = client.getOutputStream();
 		} catch (IOException e) {
@@ -37,6 +39,7 @@ public class RobotConnection {
 		byte bvalue = ivalue.byteValue();
 		byte[] data = new byte[1];
 		data[0]=bvalue;
+		//System.out.println(data);
 		try {
 			output.write(data);
 		} catch (IOException e) {
@@ -45,20 +48,24 @@ public class RobotConnection {
 		}
 	}
 	
-	public static void main(String[] args) {
+	/*public static void main(String[] args) {
 		RobotConnection rConn = new RobotConnection();
+		MOVE moves [] = {MOVE.RIGHT,MOVE.LEFT,MOVE.UP,MOVE.DOWN};
+		Random rd = new Random();
 		while(true)
 		{
-			rConn.notifyMoves(MOVE.RIGHT, null);
-			System.out.println("Notificando");
 			try {
-				Thread.sleep(1000);
+				MOVE t=moves[rd.nextInt(moves.length)];
+			rConn.notifyMoves(t, null);
+			System.out.println("Notificando: "+t);
+
+			Thread.sleep(2000);				
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
-	}
+	}*/
 	
 	
 }
